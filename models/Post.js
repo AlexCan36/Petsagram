ab94f73cfa7bdbe2909b3a9e7f10e93e290ba953
Alex Canez <el.winy.36@gmail.com>
2022-06-21 20:01:13 -0600

    first merger

---
+++ models/Post.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../controllers/connection');
+// create our Post model
+class Post extends Model {
+  static Like(body, models) {
+    return models.Like.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'image',
+          'caption',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = Like.post_id)'), 'like_count']
+          [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = Comment.post_id)'), 'comment_count']
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
+}
+
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    caption: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
